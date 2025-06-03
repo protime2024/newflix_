b91f371402913a9c1e20892669478cc8ca194f72
umair <93709449+CAPT1998@users.noreply.github.com>
2025-06-03 19:43:50 +0500

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('/'),
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
@@ -68,6 +68,11 @@ return [
     | the locations of the links and the values should be their targets.
     |
     */
+    
+     'links' => [
+        public_path('storage') => storage_path('app/public'),
+    ],
+
 
     // 'links' => [
     //     public_path('storage') => storage_path('app/public'),
