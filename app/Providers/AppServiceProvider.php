3fd9168d7be2969a0d98ee5ab9c2d371b8cea1a0
protime2024 <protime2024@gmail.com>
2025-06-03 22:40:04 +0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,8 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\App;
 class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
@@ -26,60 +27,55 @@ class AppServiceProvider extends ServiceProvider {
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void {
-        // if (!cache()->get('SystemInstalled')) {
-        //     $envFilePath = base_path('.env');
-        //     if (!file_exists($envFilePath)) {
-        //         header('Location: install');
-        //         exit;
-        //     }
-        //     $envContents = file_get_contents($envFilePath);
-        //     if (empty($envContents)) {
-        //         header('Location: install');
-        //         exit;
-        //     } else {
-        //         cache()->put('SystemInstalled', true);
-        //     }
-        // }
 
-        $viewShare['emptyMessage'] = 'Data not found';
-        $viewShare['categories']   = Category::active()->with([
-            'subcategories' => function ($subcategory) {
-                $subcategory->active();
-            },
-        ])->get(['name', 'id']);
+
+public function boot(): void {
+    $viewShare['emptyMessage'] = 'Data not found';
+
+    if (App::runningInConsole() || !Schema::hasTable('categories')) {
+        // Avoid running DB queries during artisan commands like package:discover
         view()->share($viewShare);
-
-        view()->composer('admin.partials.sidenav', function ($view) {
-            $view->with([
-                'bannedUsersCount'           => User::banned()->count(),
-                'emailUnverifiedUsersCount'  => User::emailUnverified()->count(),
-                'mobileUnverifiedUsersCount' => User::mobileUnverified()->count(),
-                'pendingTicketCount'         => SupportTicket::whereIN('status', [Status::TICKET_OPEN, Status::TICKET_REPLY])->count(),
-                'pendingDepositsCount'       => Deposit::pending()->count(),
-                'updateAvailable'            => version_compare(gs('available_version'), systemDetails()['version'], '>') ? 'v' . gs('available_version') : false,
-            ]);
-        });
-
-        view()->composer('admin.partials.topnav', function ($view) {
-            $view->with([
-                'adminNotifications'     => AdminNotification::where('is_read', Status::NO)->with('user')->orderBy('id', 'desc')->take(10)->get(),
-                'adminNotificationCount' => AdminNotification::where('is_read', Status::NO)->count(),
-            ]);
-        });
-
-        view()->composer('partials.seo', function ($view) {
-            $seo = Frontend::where('data_keys', 'seo.data')->first();
-            $view->with([
-                'seo' => $seo ? $seo->data_values : $seo,
-            ]);
-        });
-
-        if (gs('force_ssl')) {
-            \URL::forceScheme('https');
-        }
-
-        Paginator::useBootstrapFive();
-
+        return;
     }
+
+    $viewShare['categories'] = Category::active()->with([
+        'subcategories' => function ($subcategory) {
+            $subcategory->active();
+        },
+    ])->get(['name', 'id']);
+
+    view()->share($viewShare);
+
+    view()->composer('admin.partials.sidenav', function ($view) {
+        $view->with([
+            'bannedUsersCount'           => User::banned()->count(),
+            'emailUnverifiedUsersCount'  => User::emailUnverified()->count(),
+            'mobileUnverifiedUsersCount' => User::mobileUnverified()->count(),
+            'pendingTicketCount'         => SupportTicket::whereIN([Status::TICKET_OPEN, Status::TICKET_REPLY])->count(),
+            'pendingDepositsCount'       => Deposit::pending()->count(),
+            'updateAvailable'            => version_compare(gs('available_version'), systemDetails()['version'], '>') ? 'v' . gs('available_version') : false,
+        ]);
+    });
+
+    view()->composer('admin.partials.topnav', function ($view) {
+        $view->with([
+            'adminNotifications'     => AdminNotification::where('is_read', Status::NO)->with('user')->orderBy('id', 'desc')->take(10)->get(),
+            'adminNotificationCount' => AdminNotification::where('is_read', Status::NO)->count(),
+        ]);
+    });
+
+    view()->composer('partials.seo', function ($view) {
+        $seo = Frontend::where('data_keys', 'seo.data')->first();
+        $view->with([
+            'seo' => $seo ? $seo->data_values : $seo,
+        ]);
+    });
+
+    if (gs('force_ssl')) {
+        \URL::forceScheme('https');
+    }
+
+    Paginator::useBootstrapFive();
+}
+
 }
